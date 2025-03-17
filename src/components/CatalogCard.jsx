89e339d5996acdad9b64e5c1cec2f672081bc846
MuratOfficial <toimetm@gmail.com>
2025-03-17 21:26:54 +0500

    init

---
+++ src/components/CatalogCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CatalogCard() {
+  return (
+    <div>CatalogCard</div>
+  )
+}
+
+export default CatalogCard
---
+++ src/components/DropdownUser.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DropdownUser() {
+  return (
+    <div>DropdownUser</div>
+  )
+}
+
+export default DropdownUser
