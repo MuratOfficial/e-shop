02639e15dd091436cacac98f3a8c4391514430f2
MuratOfficial <toimetm@gmail.com>
2025-04-10 20:26:27 +0500

    completed basketPage

--- src/pages/BasketPage.jsx
+++ src/pages/BasketPage.jsx
@@ -5,6 +5,8 @@ import { TbSquareMinusFilled } from 'react-icons/tb';
 import { FaTrashAlt } from 'react-icons/fa';
 import BasketCard from '../components/cards/BasketCard';
 import Switch from '../components/ui/Switch'
+import Price from '../components/helper/Price';
+import { BiSolidCircleHalf } from 'react-icons/bi';
 
 function BasketPage() {
 
@@ -17,13 +19,22 @@ function BasketPage() {
     {img:'/assets/image.png', name:"Комбайн КЗС-1218 «ДЕСНА-ПОЛЕСЬЕ GS12»", price:44.50, discount:10, count:2,empty:false}
   ]
 
+  const allSumm = products.reduce((acc, curr)=>acc +(curr.price*curr.count), 0);
+  const discountSum = products.reduce((acc, curr)=> 
+    acc + ((curr.price *curr.count * curr.discount)/100)
+  , 0);
+  const summ = allSumm - discountSum;
+
+
+  const count = products?.length ?? 0;
+
   return (
     <section className=' px-32 py-6 w-full flex gap-8 flex-col min-h-screen items-start '>
       <h2 className='relative pr-8 text-7xl font-extrabold text-[#414141]'>
       Корзина
       <span className='bg-[#FF6633] font-normal rounded-sm px-2 py-1 text-white/95 text-sm absolute top-0 right-0'>3</span>
       </h2>
-      <div className="grid grid-cols-4 w-full gap-2 ">
+      <div className="grid grid-cols-4 w-full gap-16 ">
         <div className="col-span-3 flex flex-col gap-4 items-start">
 
           <div className='flex flex-row justify-start items-center'> 
@@ -42,9 +53,34 @@ function BasketPage() {
 
 
         </div>
-        <div className='flex flex-col items-start justify-start pt-12 '>
-            <div className='flex flex-row '> <Switch/> </div>
+        <div className='flex flex-col items-start gap-6 justify-start pt-12 '>
+            <div className='flex flex-row items-center gap-2 text-lg '> <Switch/> Списать <Price price={200}/> </div>
+            <p className='w-full flex flex-row items-center gap-2 text-zinc-500'>На карте накоплено <Price price={200} /></p>
+            <hr className=' text-[#F3F2F1] w-full'/>
+            <div className=' flex flex-row w-full justify-between text-zinc-500'>
+              <div className=' flex flex-col items-start gap-3'>
+                <p>{count} товар{count > 4 ? "ов" :"а"}</p>
+                <p>Скидка</p>
+              </div>
+              <div className=' flex flex-col items-start gap-3'>
+                <Price price={allSumm ??0} className={`font-semibold text-zinc-800`} />
+                <span className='text-[#FF6633] flex flex-row items-center font-bold'>-<Price price={discountSum??0} /></span>
+              </div>
 
+            </div>
+            <hr className=' text-[#F3F2F1] w-full'/>
+            <div className='flex flex-col w-full'>
+                <div className='flex flex-row w-full justify-between text-zinc-500'>
+                  <p>Итог</p>
+                  <Price className={`font-extrabold text-2xl text-zinc-800`} price={summ}/>
+              
+                </div>
+                <div className=' flex flex-row items-center justify-center text-green-500 gap-4'>
+                            <BiSolidCircleHalf size={18} className=' rotate-90' />
+                              <p className='  text-xs '>Вы получаете <span className=' font-bold'>100 бонусов</span> </p>
+                </div>
+            </div>
+            <Button className={`bg-[#FF6633] text-lg text-white/95 w-full hover:bg-green-500`} title={"Оформить заказ"}/>
         </div>
 
       </div>
